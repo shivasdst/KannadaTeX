f9e501db44def22e1345d6390618747cce5f1bd5
shivasdst <shivably04sdst@gmail.com>
2014-09-17 16:08:05 +0530

    modified README, changed file names of insengkan to install.sh and uninsengkan.sh to uninstall.sh

--- baraha/uninsengkan.sh
+++
@@ -1,9 +0,0 @@
-rm -fr  /usr/local/texlive/2011/texmf-dist/fonts/tfm/public/kantex
-rm -fr  /usr/local/texlive/2011/texmf-dist/fonts/vf/public/kantex
-rm -fr  /usr/local/texlive/2011/texmf-dist/fonts/source/public/kantex
-rm -fr  /usr/local/texlive/2011/texmf-dist/fonts/type1/public/kantex
-rm -fr  /usr/local/texlive/2011/texmf-dist/fonts/afm/public/kantex
-rm -fr  /usr/local/texlive/2011/texmf-dist/tex/latex/kantex
-
-texhash
-
---
+++ baraha/uninstall.sh
@@ -0,0 +1,9 @@
+rm -fr  /usr/local/texlive/2014/texmf-dist/fonts/tfm/public/kantex
+rm -fr  /usr/local/texlive/2014/texmf-dist/fonts/vf/public/kantex
+rm -fr  /usr/local/texlive/2014/texmf-dist/fonts/source/public/kantex
+rm -fr  /usr/local/texlive/2014/texmf-dist/fonts/type1/public/kantex
+rm -fr  /usr/local/texlive/2014/texmf-dist/fonts/afm/public/kantex
+rm -fr  /usr/local/texlive/2014/texmf-dist/tex/latex/kantex
+
+texhash
+
